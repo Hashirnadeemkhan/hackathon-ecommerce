b5f2d9c00990d63ccda84faeac7c44fc92cb679f
Hashir Nadeem Khan <khashir657@gmail.com>
2025-01-21 20:19:26 +0500

    updation

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -5,9 +5,9 @@ import { createClient } from 'next-sanity'
 
 export const client = createClient({
   projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset:"production",
+  dataset:process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion:"v2024-12-31",
-  token: process.env.SANITY_ACCESS_TOKEN,
+  token: process.env.SANITY_API_TOKEN,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
 
