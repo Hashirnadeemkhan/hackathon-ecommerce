bee15773ba11bffedc6f7705b5d1422c622b75e5
Hashir Nadeem Khan <khashir657@gmail.com>
2025-01-22 09:48:29 +0500

    updation

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,10 @@
-import { createClient } from 'next-sanity'
-import { apiVersion, dataset, projectId, token } from '../env'
+import { createClient } from "next-sanity"
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  token,
-  useCdn: true, // You can keep this true for your case
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID?.toLowerCase().replace(/[^a-z0-9-]/g, "-") || "",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  apiVersion: "2024-02-22",
+  token: process.env.SANITY_API_TOKEN,
+  useCdn: true,
 })
+
