88f1fe303a8c0f0766619fafc5e4b8ba76ac2236
Hashir Nadeem Khan <khashir657@gmail.com>
2025-01-22 09:19:08 +0500

    updation

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,5 +1,4 @@
-export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-31'
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-12-31'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
@@ -11,10 +10,14 @@ export const projectId = assertValue(
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
+export const token = assertValue(
+  process.env.SANITY_API_TOKEN,
+  'Missing environment variable: SANITY_API_TOKEN'
+)
+
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage)
   }
-
   return v
 }
